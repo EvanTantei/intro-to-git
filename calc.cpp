2f8c6e809f76a7a570a2d12a3b70655b87d7fbaa
EvanTantei <evanlim991@gmail.com>
2025-09-01 16:51:47 +0700

    Commit pertama

---
+++ calc.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	int a, b;
+	a = 25;
+	b = 7;
+	printf("Penjumlahan dari a dan b adalah: %d\n", a + b);
+	printf("Pengurangan dari a dan b adalah: %d\n", a - b);
+	printf("Perkalian dari a dan b adalah: %d\n", a * b);
+	printf("Pembagian dari a dan b adalah: %f\n", (float) a / b);
+}
